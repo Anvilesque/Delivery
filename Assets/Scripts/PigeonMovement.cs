6e8297d8b0d5d74f6f8f9c14fdcd0dea8969ff85
Daniel Kao <dack8@outlook.com>
2023-04-28 22:12:17 -0700

    add dash ability

--- Assets/Scripts/PigeonMovement.cs
+++ Assets/Scripts/PigeonMovement.cs
@@ -7,6 +7,8 @@ public class PigeonMovement : MonoBehaviour
     private Vector2 pigeonVelocity;
     private float pigonSpeed = 5.0f;
     private Rigidbody2D rb;
+    private int dashCooldown = 0;
+    private int durationTimer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,27 @@ public class PigeonMovement : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        if(Input.GetButton("Fire1") && dashCooldown == 0)
+        {
+            pigonSpeed = 20.0f;
+            dashCooldown = GetTime(4);
+            durationTimer = 3;
+        }
         rb.MovePosition(transform.position + input * Time.deltaTime *pigonSpeed);
+        if(durationTimer == 0)
+            pigonSpeed = 5.0f;
+        UpdateCooldowns();
+    }
+
+    public int GetTime(int seconds)
+    {
+        return seconds*60;
+    }
+    public void UpdateCooldowns()
+    {
+        if (dashCooldown > 0)
+            dashCooldown--;
+        if (durationTimer > 0)
+            durationTimer--;
     }
 }
