dcb0d1634f5e9a8997dbc5d5a1b4c8dd34367da4
Daniel Kao <dack8@outlook.com>
2023-04-28 23:14:43 -0700

    add speed boost ability and make sneak change detection radius

--- Assets/Scripts/PigeonMovement.cs
+++ Assets/Scripts/PigeonMovement.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class PigeonMovement : MonoBehaviour
 {
+    public GuardDetect guard;
     private Vector2 pigeonVelocity;
     private float pigeonSpeed = 5.0f;
     private Rigidbody2D rb;
     private int dashCooldown = 0;
     private int durationTimer = 0;
-    private int sneakCooldown = 0;
+    private int speedCooldown = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +20,24 @@ public class PigeonMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        guard.detectRadius = 2f;
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        if(Input.GetButton("Fire1") && dashCooldown == 0  && durationTimer == 0)
+        if(Input.GetButton("Dash") && dashCooldown == 0  && durationTimer == 0)
         {
             pigeonSpeed = 20.0f;
             dashCooldown = GetTime(5);
             durationTimer = 3;
         }
-        else if(Input.GetButton("Fire2") && durationTimer == 0)
+        else if(Input.GetButton("SpeedBoost") && speedCooldown == 0 && durationTimer == 0)
         {
-            pigeonSpeed = 0;
+            pigeonSpeed = 7.5f;
+            durationTimer = GetTime(4);
+            speedCooldown = GetTime(20);
         }
-        else if(Input.GetButton("Fire3") && durationTimer == 0)
+        else if(Input.GetButton("Sneak") && durationTimer == 0)
         {
             pigeonSpeed = 2.5f;
+            guard.detectRadius = 1f;
         }
         rb.MovePosition(transform.position + input * Time.deltaTime *pigeonSpeed);
         if(durationTimer == 0)
@@ -48,6 +53,8 @@ public class PigeonMovement : MonoBehaviour
     {
         if (dashCooldown > 0)
             dashCooldown--;
+        if (speedCooldown > 0)
+            speedCooldown--;
         if (durationTimer > 0)
             durationTimer--;
     }
