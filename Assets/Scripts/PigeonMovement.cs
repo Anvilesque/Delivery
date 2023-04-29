83ca91e4df311959f13895e7540162313dc60c91
Daniel Kao <dack8@outlook.com>
2023-04-28 21:51:25 -0700

    pigeon movement update + walls/floors of maze

--- Assets/Scripts/PigeonMovement.cs
+++ Assets/Scripts/PigeonMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PigeonMovement : MonoBehaviour
 {
     private Vector2 pigeonVelocity;
-    private float pigonSpeed = 10.0f;
+    private float pigonSpeed = 5.0f;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -14,10 +14,9 @@ public class PigeonMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        pigeonVelocity.x = Input.GetAxis("Horizontal")*pigonSpeed;
-        pigeonVelocity.y = Input.GetAxis("Vertical")*pigonSpeed;
-        rb.velocity = pigeonVelocity;
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        rb.MovePosition(transform.position + input * Time.deltaTime *pigonSpeed);
     }
 }
